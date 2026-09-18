dbd2435ea428be315ad77c30a1bb75a36831b1a0
dlfirestone <133285465+dlfirestone@users.noreply.github.com>
2026-09-18 00:41:27 -0400

    start main app UI layout

--- Ethicly/Ethicly/ContentView.swift
+++ Ethicly/Ethicly/ContentView.swift
@@ -12,13 +12,46 @@ struct ContentView: View {
   let quote = QuoteManager.shared.currentQuote()
   
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+    ZStack {
+      Color("Paper")
+        .ignoresSafeArea()
+      
+      VStack(alignment: .leading, spacing: 10) {
+        Text("Ethics quote of the day:")
+          .font(.headline)
+          .padding(.bottom, 50)
+        
+        Text(quote.text)
+          .font(.title)
+          .fontDesign(.serif)
+          .multilineTextAlignment(.center)
+        
+        Divider()
+          .frame(width: 140)
+          .background(.secondary)
+          .frame(maxWidth: .infinity, alignment: .center)
+          .padding(.vertical, 20)
+        
+        VStack(alignment: .trailing) {
+          if (quote.author != nil) {
+            Text(quote.author!)
+          }
+          
+          if (quote.source != nil) {
+            Text(quote.source!)
+              .italic()
+          }
+          
+          if (quote.year != nil) {
+            let yearText = quote.year.map(String.init) ?? ""
+            Text(yearText)
+          }
+        }
+        .frame(maxWidth: .infinity, alignment: .trailing)
+        .padding(.trailing, 30)
+      }
+      .padding(.horizontal, 30)
     }
-    .padding()
   }
 }
 
